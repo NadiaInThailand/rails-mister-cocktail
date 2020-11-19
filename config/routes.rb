6e581f100d99fb4ad7fae22223b08a62dedb13ae
nadia al-jasem <nadiajasem@gmail.com>
2020-11-19 17:45:38 +0700

    can add/view cocktails and doses but no styling yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/edit'
+  # get 'doses/update'
+  # get 'doses/destroy'
+  # get 'ingredients/index'
+  # get 'ingredients/new'
+  # get 'ingredients/create'
+  # get 'ingredients/edit'
+  # get 'ingredients/update'
+  # get 'ingredients/destroy'
+  #get 'cocktails/index'
+  #get 'cocktails/show'
+  #get 'cocktails/new'
+  #get 'cocktails/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :ingredients, only: [:index, :new, :create, :edit, :update, :destroy]
+
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# if Rails.env.development?
+#   puts "Seeding..."
+#   10.times do
+#     c = Cocktail.create!(
+#       name:
+#     puts "Restaurant id #{c.id} created."
+#   end
+
+#   puts "Seeding done"
+# end
+
+require 'open-uri'
+require 'json'
+
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+# file = open(url).read
+# readable_file = JSON.parse(file)
+
+# readable_file["drinks"].each do |item|
+#     Cocktail.create!(
+#       name: item["strDrink"]
+#     )
+# end
+
+ingredients_url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_file = open(ingredients_url).read
+ingredients = JSON.parse(ingredients_file)
+
+ingredients['drinks'].each do |drink|
+  Ingredient.create!(name: drink["strIngredient1"])
+end
+
+
